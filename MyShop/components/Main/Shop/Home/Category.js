3d3683dfe5f37950e8372239024e9bc9037f2f14
Ly Nguyen <nmly68it@gmail.com>
2020-02-27 00:57:43 +0700

    Add Category.js

--- MyShop/components/Main/Shop/Home/Category.js
+++ MyShop/components/Main/Shop/Home/Category.js
@@ -1,19 +1,33 @@
 import React, { Component } from "react";
-import { Image, View, Text, Dimensions, StyleSheet } from 'react-native';
-import bannerImage from './../../../../assets/media/temp/banner.jpg';
+import { Image, View, Text, Dimensions, StyleSheet, ImageBackground } from 'react-native';
+import Swiper from 'react-native-swiper';
+
+import littleIcon from './../../../../assets/media/temp/little.jpg';
+import maxiIcon from './../../../../assets/media/temp/maxi.jpg';
+import partyIcon from './../../../../assets/media/temp/party.jpg';
 
 const { height, width } = Dimensions.get('window');
 
 export default class Category extends Component {
     render() {
-        const { wrapper, textStyle, imageStyle } = styles;
+        const { wrapper, textStyle, imageStyle, categoryTitleStyle } = styles;
         return (
-            <View style={wrapper}>
+            <View style={wrapper} >
                 <View style={{ flex: 1, justifyContent: 'center' }}>
-                    <Text style={textStyle}>SPRING COLLECTION</Text>
+                    <Text style={textStyle}>LIST OF CATEGORY</Text>
                 </View>
                 <View style={{ flex: 4 }}>
-                    <Image source={bannerImage} style={imageStyle}/>
+                    <Swiper autoplay={true}>
+                        <ImageBackground source={littleIcon} style={imageStyle}>
+                            <Text style={categoryTitleStyle}>Maxi Dress</Text>
+                        </ImageBackground>
+                        <ImageBackground source={maxiIcon} style={imageStyle}>
+                            <Text style={categoryTitleStyle}>Maxi Dress</Text>
+                        </ImageBackground>
+                        <ImageBackground source={partyIcon} style={imageStyle}>
+                            <Text style={categoryTitleStyle}>Maxi Dress</Text>
+                        </ImageBackground>                        
+                    </Swiper>
                 </View>
             </View>
         );
@@ -31,15 +45,22 @@ const styles = StyleSheet.create({
         shadowColor: '#2E272B',
         shadowOffset: { width: 0, height: 3 },
         shadowOpacity: 0.2,
-        padding: 10, 
+        padding: 10,
         paddingTop: 0
-    }, 
-    textStyle : {
+    },
+    textStyle: {
         fontSize: 20,
         color: '#AFAEAF'
-    }, 
-    imageStyle : {
+    },
+    imageStyle: {
         height: imageHeight,
-        width: imageWidth
+        width: imageWidth,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    categoryTitleStyle: {
+        fontSize: 20,
+        fontFamily: 'Avenir',
+        color: '#9A9A9A'
     }
 });
