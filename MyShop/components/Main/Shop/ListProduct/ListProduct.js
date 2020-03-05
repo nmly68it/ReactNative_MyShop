65eb6b7797974c1e5666bf34d6727f736a59ce72
Ly Nguyen <nmly68it@gmail.com>
2020-03-06 00:40:32 +0700

    List product

--- MyShop/components/Main/Shop/ListProduct/ListProduct.js
+++ MyShop/components/Main/Shop/ListProduct/ListProduct.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, ScrollView, Image } from 'react-native';
+
+import backListIcon from '../../../../assets/media/appIcon/backList.png';
+import sp1 from '../../../../assets/media/temp/sp1.jpeg';
+import sp2 from '../../../../assets/media/temp/sp2.jpeg';
+import sp3 from '../../../../assets/media/temp/sp3.jpeg';
 
 export default class ListProduct extends Component {
     goBack = () => {
@@ -11,15 +16,143 @@ export default class ListProduct extends Component {
     }
 
     render() {
+        const {
+            container, header, wrapper,
+            backIconStyle, titleStyle, productContainer,
+            productInfo, productImage, lastRowInfo,
+            txtName, txtPrice, txtMaterial, txtColor, txtShowDetail
+        } = styles;
         return (
-            <View style={{ flex: 1, backgroundColor: 'green' }}>
-                <TouchableOpacity onPress={this.goBack}>
-                    <Text>Go Back</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={this.goToProductDetail}>
-                    <Text>Go To Product Details</Text>
-                </TouchableOpacity>
+            <View style={container}>
+                <ScrollView style={wrapper}>
+                    <View style={header}>
+                        <TouchableOpacity onPress={this.goBack}>
+                            <Image source={backListIcon} style={backIconStyle} />
+                        </TouchableOpacity>
+                        <Text style={titleStyle}>Party Dress</Text>
+                        <View style={{ width: 30 }} />
+                    </View>
+                    <View style={productContainer}>
+                        <Image source={sp1} style={productImage} />
+                        <View style={productInfo}>
+                            <Text style={txtName}>Lance Sleeve Si</Text>
+                            <Text style={txtPrice}>117$</Text>
+                            <Text style={txtMaterial}>Material silk</Text>
+                            <View style={lastRowInfo}>
+                                <Text style={txtColor}>Colo RoyalBlue</Text>
+                                <View style={{ backgroundColor: 'cyan', width: 16, height: 16, borderRadius: 8 }} />
+                                <TouchableOpacity>
+                                    <Text style={txtShowDetail}>SHOW DETAILS</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    </View>
+                    <View style={productContainer}>
+                        <Image source={sp2} style={productImage} />
+                        <View style={productInfo}>
+                            <Text style={txtName}>Lance Sleeve Si</Text>
+                            <Text style={txtPrice}>117$</Text>
+                            <Text style={txtMaterial}>Material silk</Text>
+                            <View style={lastRowInfo}>
+                                <Text style={txtColor}>Colo RoyalBlue</Text>
+                                <View style={{ backgroundColor: 'cyan', width: 16, height: 16, borderRadius: 8 }} />
+                                <TouchableOpacity>
+                                    <Text style={txtShowDetail}>SHOW DETAILS</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    </View>
+                    <View style={productContainer}>
+                        <Image source={sp3} style={productImage} />
+                        <View style={productInfo}>
+                            <Text style={txtName}>Lance Sleeve Si</Text>
+                            <Text style={txtPrice}>117$</Text>
+                            <Text style={txtMaterial}>Material silk</Text>
+                            <View style={lastRowInfo}>
+                                <Text style={txtColor}>Colo RoyalBlue</Text>
+                                <View style={{ backgroundColor: 'cyan', width: 16, height: 16, borderRadius: 8 }} />
+                                <TouchableOpacity>
+                                    <Text style={txtShowDetail}>SHOW DETAILS</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    </View>
+                </ScrollView>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#DBDBD8',
+        padding: 10
+    },
+    header: {
+        height: 50,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 5
+        //backgroundColor: '#FFF'
+    },
+    wrapper: {
+        backgroundColor: '#FFF',
+        shadowColor: '#2E272B',
+        shadowOffset: { width: 0, height: 3 },
+        shadowOpacity: 0.2,
+        paddingHorizontal: 10
+    },
+    backIconStyle: {
+        width: 30,
+        height: 30
+    },
+    titleStyle: {
+        fontFamily: 'Avenir',
+        color: '#B10D65',
+        fontSize: 20
+    },
+    productContainer: {
+        flexDirection: 'row',
+        paddingVertical: 15,
+        borderTopColor: '#F0F0F0',
+        borderTopWidth: 1
+    },
+    productInfo: {
+        justifyContent: "space-between",
+        marginLeft: 15,
+        flex: 1,
+    },
+    productImage: {
+        width: 90,
+        height: (90 * 452) / 361
+    },
+    lastRowInfo: {
+        flexDirection: 'row',
+        justifyContent: "space-between",
+        alignItems: "center"
+    },
+    txtName: {
+        fontFamily: 'Avenir',
+        color: '#BCBCBC',
+        fontSize: 20,
+        fontWeight: '400'
+
+    },
+    txtPrice: {
+        fontFamily: 'Avenir',
+        color: '#B10D65'
+    },
+    txtMaterial: {
+        fontFamily: 'Avenir',
+    },
+    txtColor: {
+        fontFamily: 'Avenir',
+    },
+    txtShowDetail: {
+        fontFamily: 'Avenir',
+        color: '#B10D65',
+        fontSize: 11
+    }
+});
